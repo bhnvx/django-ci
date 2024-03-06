15e832b4afb9ffe22a0028bfc351159a5281eaf4
bhnvx <xzt______@naver.com>
2024-03-06 10:59:45 +0900

    init

--- bhnvx/settings_/base.py
+++ bhnvx/settings_/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-1zr@t2!%gv=!_&58+as%p8-^eol9!yt3v4=cr%iq8==5z5t@wi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
